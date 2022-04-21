f9c186d31f6c022f8ce93200a43b7b7e38383983
liuyang <1714610279@qq.com>
2022-04-21 22:51:05 +0800

    加入今日诗词功能

---
+++ themes/hexo-theme-matery/source/js/mine.js
@@ -0,0 +1,9 @@
+function loadPoem(result) {
+  let sentence = document.querySelector("#poem_sentence")
+  let info = document.querySelector("#poem_info")
+  sentence.textContent = result.data.content
+  info.textContent = '【' + result.data.origin.dynasty + '】' + result.data.origin.author + '《' + result.data.origin.title + '》'
+}
+jinrishici.load(loadPoem);
+
+
